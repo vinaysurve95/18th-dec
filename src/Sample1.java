0148c5619bf171abaf684f08c51d1670f23ef375
vinodsurve90 <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>
2022-04-30 14:21:59 +0530

    [vinay s]:change the code line no 8

--- src/Sample1.java
+++ src/Sample1.java
@@ -4,6 +4,15 @@ public class Sample1 {
 	public static void main(String[] args) {
       
 		System.out.println("Good Morning");
+		String eat="food";
+		if(eat=="food")
+		{
+			System.out.println("Feeling better");
+		}
+		else
+		{
+			System.out.println("Not work properly");
+		}
 	}
 
 }
